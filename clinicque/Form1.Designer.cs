ddb1b397dbd79a09079aefe109eb5777120139d9
besagasandrei-code <besagasandrei@gmail.com>
2026-08-25 15:04:56 +0800

    almost done

--- clinicque/Form1.Designer.cs
+++ clinicque/Form1.Designer.cs
@@ -28,21 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.reasonbox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.ptypebox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.agebox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // namebox
+            // 
+            this.namebox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.namebox.Location = new System.Drawing.Point(62, 23);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(187, 20);
+            this.namebox.TabIndex = 1;
+            this.namebox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.namebox.TextChanged += new System.EventHandler(this.namebox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Reason for Visit";
+            // 
+            // reasonbox
+            // 
+            this.reasonbox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.reasonbox.Location = new System.Drawing.Point(447, 23);
+            this.reasonbox.Name = "reasonbox";
+            this.reasonbox.Size = new System.Drawing.Size(100, 20);
+            this.reasonbox.TabIndex = 5;
+            this.reasonbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.reasonbox.TextChanged += new System.EventHandler(this.reasonbox_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(582, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Patient Type";
+            // 
+            // ptypebox
+            // 
+            this.ptypebox.BackColor = System.Drawing.SystemColors.Window;
+            this.ptypebox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ptypebox.FormattingEnabled = true;
+            this.ptypebox.Items.AddRange(new object[] {
+            "Regular",
+            "Senior",
+            "Pregnant",
+            "Emergency"});
+            this.ptypebox.Location = new System.Drawing.Point(655, 23);
+            this.ptypebox.Name = "ptypebox";
+            this.ptypebox.Size = new System.Drawing.Size(121, 21);
+            this.ptypebox.TabIndex = 8;
+            this.ptypebox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(442, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 59);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Add To Que";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F);
+            this.label5.Location = new System.Drawing.Point(285, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(286, 32);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Patients Que Number";
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.BackColor = System.Drawing.Color.Red;
+            this.ClearBtn.ForeColor = System.Drawing.Color.White;
+            this.ClearBtn.Location = new System.Drawing.Point(228, 62);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(105, 59);
+            this.ClearBtn.TabIndex = 12;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = false;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 250);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(469, 106);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // agebox
+            // 
+            this.agebox.Location = new System.Drawing.Point(307, 26);
+            this.agebox.Name = "agebox";
+            this.agebox.Size = new System.Drawing.Size(26, 20);
+            this.agebox.TabIndex = 14;
+            this.agebox.TextChanged += new System.EventHandler(this.agebox_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.agebox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ptypebox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.reasonbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.namebox);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox reasonbox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox ptypebox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox agebox;
     }
 }
 
--- clinicque/Form1.cs
+++ clinicque/Form1.cs
@@ -12,12 +12,51 @@ namespace clinicque
 {
     public partial class Form1 : Form
     {
+        string name;
+        double age = 0;
+        string reason;
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void namebox_TextChanged(object sender, EventArgs e)
+        {
+            name = namebox.Text;
+        }
+
+        private void reasonbox_TextChanged(object sender, EventArgs e)
+        {
+            reason = reasonbox.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+         }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void agebox_TextChanged(object sender, EventArgs e)
         {
 
         }
